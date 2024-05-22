690c7dc42f086e39160fd76e6b064ab40030acc5
Shaun Offenbacher <makeiteasierapps@gmail.com>
2024-05-22 13:47:48 -0600

    stash

--- client/ios/FriendTests/FriendTests.m
+++ client/ios/FriendTests/FriendTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface PaxxiumTests : XCTestCase
+@interface FriendTests : XCTestCase
 
 @end
 
-@implementation PaxxiumTests
+@implementation FriendTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {

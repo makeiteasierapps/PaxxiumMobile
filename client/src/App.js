e95c0613ac9725d4c2131ee0cc860a2b31496a79
Shaun Offenbacher <makeiteasierapps@gmail.com>
2024-05-23 15:36:23 -0600

    we back boyssss

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,7 @@ import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {AuthContext, AuthProvider} from './contexts/AuthContext';
 import {MomentsProvider} from './contexts/MomentsContext';
 import {ChatProvider} from './contexts/ChatContext';
-
+import {BluetoothProvider} from './contexts/BluetoothContext';
 import {SnackbarProvider} from './contexts/SnackbarContext';
 import AuthScreen from './screens/AuthScreen';
 import MainScreen from './screens/MainScreen';
@@ -51,11 +51,13 @@ const App = () => {
 export default () => (
   <SnackbarProvider>
     <AuthProvider>
-      <MomentsProvider>
-        <ChatProvider>
-          <App />
-        </ChatProvider>
-      </MomentsProvider>
+      <BluetoothProvider>
+        <MomentsProvider>
+          <ChatProvider>
+            <App />
+          </ChatProvider>
+        </MomentsProvider>
+      </BluetoothProvider>
       <MySnackBar />
     </AuthProvider>
   </SnackbarProvider>

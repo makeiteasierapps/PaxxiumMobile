eaa5560e069ea74249e8317ad2122d04426432a3
Shaun Offenbacher <makeiteasierapps@gmail.com>
2024-05-25 20:07:00 -0600

    refactored sam main

--- server/functions/sam/main.py
+++ server/functions/sam/main.py
@@ -1,8 +1,7 @@
 import os
-from flask import Response, send_file
+from flask import send_file
 from dotenv import load_dotenv
 
-headers = {"Access-Control-Allow-Origin": "*"}
 load_dotenv()
 
 # Google clouds file structure is different
@@ -11,18 +10,17 @@ if os.getenv('LOCAL_DEV') == 'True':
 else:
     from BossAgent import BossAgent
 
+headers = {"Access-Control-Allow-Origin": "*"}
 def cors_preflight_response():
-    cors_headers = {
+    return ("", 204, {
         "Access-Control-Allow-Origin": "*",
         "Access-Control-Allow-Methods": "GET, POST, OPTIONS, DELETE, PUT, PATCH",
         "Access-Control-Allow-Headers": "Authorization, Content-Type, Project-ID",
         "Access-Control-Max-Age": "3600",
-    }
-    return ("", 204, cors_headers)
+    })
 
 def handle_new_message(request):
-    data = request.json
-    new_message = data['newMessage']
+    new_message = request.json['newMessage']
     boss_agent = BossAgent()
     get_text_response = boss_agent.get_full_response(new_message)
     boss_agent.stream_audio_response(get_text_response)

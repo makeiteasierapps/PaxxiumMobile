fb7dbbf58ee499203030c5de26e9626145d83bf6
Shaun Offenbacher <makeiteasierapps@gmail.com>
2024-05-29 14:39:27 -0600

    improved tts pipeline

--- client/src/components/sam/AudioCircle.js
+++ client/src/components/sam/AudioCircle.js
@@ -1,4 +1,4 @@
-import React, {useState, useRef, useEffect} from 'react';
+import {useState, useRef, useCallback, useEffect} from 'react';
 import {View, StyleSheet, TouchableOpacity, Text} from 'react-native';
 import useAudioStream from '../../hooks/useAudioStream';
 import Sound from 'react-native-sound';
@@ -11,7 +11,6 @@ import {useFocusEffect} from '@react-navigation/native';
 import {BACKEND_URL, BACKEND_URL_PROD} from '@env';
 
 const AudioCircle = () => {
-  const [circleSize, setCircleSize] = useState(200);
   const [displayText, setDisplayText] = useState('');
   const wordDetected = useRef(false);
   const porcupineRef = useRef(null);
@@ -22,61 +21,74 @@ const AudioCircle = () => {
       : `${BACKEND_URL_PROD}`;
 
   const processErrorCallback = error => {
-    console.error(error);
+    console.error('Porcupine Error:', error);
+  };
+
+  const playSound = (filePath, basePath = Sound.MAIN_BUNDLE, onComplete) => {
+    console.log('Initializing sound playback');
+    Sound.setCategory('Playback');
+    const sound = new Sound(filePath, basePath, error => {
+      if (error) {
+        console.error('Failed to load the sound', error);
+        return;
+      }
+      sound.setVolume(1);
+      sound.play(success => {
+        if (!success) {
+          console.error('Sound playback failed');
+        } else {
+          console.log('Sound playback succeeded');
+          if (onComplete) {
+            onComplete();
+          }
+        }
+        console.log('Releasing sound resource');
+        sound.release();
+      });
+    });
   };
 
   const detectionCallback = async keywordIndex => {
-    if (porcupineRef.current) {
+    if (porcupineRef.current && keywordIndex >= 0) {
       try {
-        if (keywordIndex >= 0) {
-          console.log('Wake word detected!');
-          Sound.setCategory('Playback');
-          const sound = new Sound(
-            'greeting1Nova.mp3',
-            Sound.MAIN_BUNDLE,
-            error => {
-              if (error) {
-                console.error('Failed to load the sound', error);
-                return;
-              }
-              sound.setVolume(1);
-              sound.play(success => {
-                if (!success) {
-                  console.error('Sound playback failed');
-                }
-              });
-            },
-          );
-        }
+        playSound('greeting1Nova.mp3', Sound.MAIN_BUNDLE, () => {
+          startRecording();
+          porcupineRef.current.stop().then(() => {
+            porcupineRef.current.delete();
+          });
+        });
       } catch (e) {
         console.error('Error processing audio frame', e);
       }
     }
   };
 
-  useFocusEffect(
-    React.useCallback(() => {
-      const initPorcupine = async () => {
-        try {
-          const accessKey = process.env.PICO_ACCESS_KEY; // Your Picovoice access key
-          porcupineRef.current = await PorcupineManager.fromBuiltInKeywords(
-            accessKey,
-            [BuiltInKeywords.COMPUTER],
-            detectionCallback,
-            processErrorCallback,
-          );
-          await porcupineRef.current.start();
-        } catch (err) {
-          console.error('Failed to initialize Porcupine', err);
-        }
-      };
+  const initPorcupine = async () => {
+    try {
+      const accessKey = process.env.PICO_ACCESS_KEY;
+      porcupineRef.current = await PorcupineManager.fromBuiltInKeywords(
+        accessKey,
+        [BuiltInKeywords.COMPUTER],
+        detectionCallback,
+        processErrorCallback,
+      );
+      await porcupineRef.current.start();
+    } catch (err) {
+      console.error('Failed to initialize Porcupine', err);
+    }
+  };
 
+  useFocusEffect(
+    useCallback(() => {
+      console.log('Component focused, initializing Porcupine');
       initPorcupine();
 
       return () => {
+        console.log('Component lost focus, cleaning up');
         if (porcupineRef.current) {
           porcupineRef.current.stop().then(() => {
             porcupineRef.current.delete();
+            stopRecording();
           });
         }
       };
@@ -89,9 +101,8 @@ const AudioCircle = () => {
   };
 
   const handleSilenceDetected = async message => {
-    console.log(wordDetected.current);
     if (wordDetected.current) {
-      console.log('Silence detected sending message:', message);
+      console.log('Silence detected, sending message:', message);
       wordDetected.current = false;
       try {
         const response = await fetch(`${samUrl}/sam`, {
@@ -100,32 +111,20 @@ const AudioCircle = () => {
             'Content-Type': 'application/json',
             'X-API-Key': API_KEY,
           },
-          body: JSON.stringify({
-            newMessage: message,
-          }),
+          body: JSON.stringify({newMessage: message}),
         });
 
         if (response.ok) {
           setDisplayText('');
+          stopRecording();
           const blob = await response.blob();
           const reader = new FileReader();
           reader.onloadend = async () => {
             const base64data = reader.result;
             const path = `${RNFS.DocumentDirectoryPath}/audio.mp3`;
             await RNFS.writeFile(path, base64data.split(',')[1], 'base64');
-            console.log('File written to:', path);
-            Sound.setCategory('Playback');
-            const sound = new Sound(path, null, error => {
-              if (error) {
-                console.error('Failed to load the sound', error);
-                return;
-              }
-              sound.setVolume(1);
-              sound.play(success => {
-                if (!success) {
-                  console.error('Sound playback failed');
-                }
-              });
+            playSound(path, null, () => {
+              startRecording();
             });
           };
           reader.readAsDataURL(blob);
@@ -138,28 +137,15 @@ const AudioCircle = () => {
     }
   };
 
-  const {startRecording, stopRecording, isRecording} = useAudioStream(
+  const {startRecording, stopRecording} = useAudioStream(
     handleWordDetected,
     handleSilenceDetected,
     setDisplayText,
   );
 
-  const handlePress = () => {
-    if (isRecording) {
-      setCircleSize(200);
-      stopRecording();
-      return;
-    }
-    setCircleSize(prevSize => (prevSize === 200 ? 300 : 200));
-    startRecording();
-  };
-
   return (
     <View>
-      <TouchableOpacity
-        style={[styles.circle, {width: circleSize, height: circleSize}]}
-        onPress={handlePress}
-      />
+      <TouchableOpacity style={[styles.circle, {width: 200, height: 200}]} />
       <Text>{displayText}</Text>
     </View>
   );

2f030800f1165d03916f2cf053c4cb25475bcf03
Shaun Offenbacher <makeiteasierapps@gmail.com>
2024-05-30 09:53:42 -0600

    successful build with native swift module

--- client/ios/Friend/AppDelegate.mm
+++ client/ios/Friend/AppDelegate.mm
@@ -1,13 +1,26 @@
+#define RCT_DEBUG 1
+
 #import "AppDelegate.h"
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTBridge.h>
-#import <React/RCTBridgeModule.h>
+#import <React/RCTRootView.h>
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
+  RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
+                                                   moduleName:@"Friend" // Replace with your app's module name
+                                            initialProperties:nil];
+
+  rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
+
+  self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+  UIViewController *rootViewController = [UIViewController new];
+  rootViewController.view = rootView;
+  self.window.rootViewController = rootViewController;
+  [self.window makeKeyAndVisible];
   return YES;
 }
 
